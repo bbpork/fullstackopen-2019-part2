2c258ceacf8e76b2ca693bf413a0ded30c3490c1
bbpork <bbpork1018@gmail.com>
2019-10-23 20:15:46 +0800

    using inline style instead of index.css

--- phonebook/src/components/Notification.js
+++ phonebook/src/components/Notification.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Notification = ({ notification }) => {
   if (notification === null) return null;
-  const className = `notification notification-${notification.type}`;
-  return <div className={className}>{notification.message}</div>;
+  const color = notification.type === "successful" ? "green" : "red";
+  const style = {
+    border: `2px solid ${color}`,
+    color: color,
+    borderRadius: "0.4rem",
+    fontSize: "1.2rem",
+    margin: "1rem auto",
+    padding: "0.8rem"
+  };
+  return <div style={style}>{notification.message}</div>;
 };
 
 export default Notification;
